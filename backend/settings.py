88a9df716ac2907906088a8c26db0b7b47c89a66
Ankit Sthapit <ankit@Ankits-MacBook-Air.local>
2024-06-21 01:10:12 +0545

    added frontend in cors

--- backend/settings.py
+++ backend/settings.py
@@ -175,6 +175,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
+    'https://d4-nepal-frontend.vercel.app'
 ]
 
 CORS_ALLOWS_CREDENTIALS = True
